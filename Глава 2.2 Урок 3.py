5ec7fe27544cde42c855350b3bdc7cf3f83e0a4d
QA-Vlad <kuznetcov.vladlen@gmail.com>
2024-04-18 19:12:43 +0700

    Первый коммит

--- Глава 2.2 Урок 3.py
+++ Глава 2.2 Урок 3.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 import time
 
+
 try:
     browser = webdriver.Chrome()
     browser.get("https://suninjuly.github.io/selects1.html")
