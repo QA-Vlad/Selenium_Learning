17f776717ee5a325262dac8adfcb6def50445e75
QA-Vlad <kuznetcov.vladlen@gmail.com>
2024-04-27 20:00:36 +0700

    fix assert in test_items.py

--- Stepik/test_items.py
+++ Stepik/test_items.py
@@ -1,4 +1,7 @@
+from selenium.common import TimeoutException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 link = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -6,6 +9,18 @@ link = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_check_buy_button(browser):
     browser.get(link)
-    buy_button = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
-    assert buy_button.is_displayed()
-    time.sleep(30)
+
+    try:
+        # Явное ожидание появления кнопки на странице
+        buy_button = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "btn-add-to-basket"))
+        )
+    except TimeoutException:
+        # Если элемент не был найден за 10 секунд, выводим соответствующее сообщение
+        print(f"Элемент с селектором {By.CLASS_NAME, 'btn-add-to-basket'} не был найден на странице {link}")
+        raise
+    else:
+        # Проверка, что кнопка отображается
+        assert buy_button.is_displayed(), "Кнопка 'Добавить в корзину' не отображается"
+
+    time.sleep(30)  # Визуальная проверка языка сайта
